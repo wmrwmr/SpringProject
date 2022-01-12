2db870d8e66fc2480d5bfff1015c55dfcaa059f6
wmrwmr <wnrak0116@gmail.com>
2022-01-12 19:42:12 +0900

    2022-01-12
    1. Mapper 인터페이스와 XML을 이용한 CRUD 구현(영속 계층)
    2. 위에서 작성한 Mapper 객체 및 Service 인터페이스, ServiceImpl 클래스를 이용한 CRUD 구현(비즈니스 계층, @Service)

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -2,7 +2,6 @@ package org.zerock.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
 import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
@@ -10,4 +9,13 @@ public interface BoardMapper {
 //	@Select("select * from tbl_board where bno > 0") -> BorderMapper.xml 로 처리완료
 	public List<BoardVO> getList();
 	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
+	
+	public int delete(Long bno);
+	
+	public int update(BoardVO board);
 }
