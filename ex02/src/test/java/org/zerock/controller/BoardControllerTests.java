cd2adde2d240330202f7fc04a2b4f12cb57184b7
wmrwmr <wnrak0116@gmail.com>
2022-01-18 18:15:01 +0900

    2022-01-18
    1. 데이터베이스 페이징 처리(SQL 힌트, ROWNUM, order by 보다는 인덱스, 인라인뷰)
    2. 위 내용 Mybatis와 스프링에 적용

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -59,13 +59,13 @@ public class BoardControllerTests {
 	}
 	
 	
-	@Test
-	public void testModify() throws Exception {
-		
-		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1").param("title", "수정된 테스트 새 글 제목").param("content", "수정된 테스트 새 글 내용").param("writer", "user00")).andReturn().getModelAndView().getViewName();
-		
-		log.info(resultPage);
-	}
+//	@Test
+//	public void testModify() throws Exception {
+//		
+//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1").param("title", "수정된 테스트 새 글 제목").param("content", "수정된 테스트 새 글 내용").param("writer", "user00")).andReturn().getModelAndView().getViewName();
+//		
+//		log.info(resultPage);
+//	}
 	
 	
 //	@Test
